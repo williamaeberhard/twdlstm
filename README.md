cd9951dd432b499b2861d08027b6242d0b2d648c
William Aeberhard <william.aeberhard@gmail.com>
2025-03-20 10:24:04 +0000

    v0.2

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ twdlstm: PyTorch code for training an LSTM NN on TWD_norm series
 
 ### TODO
 
-* [ ] loop over series list in config yaml, create one array for all data
+* [x] loop over series list in config yaml, create one array for all data
 * [ ] add in config whether to use CPU or GPU
 
 
 ### Version history
 
-This is twdlstm version 0.1.1. Change log:
+This is twdlstm version 0.2. Change log:
+* v0.2: arbitrary number of series can be supplied (>=2), loop over seriesvec
 * v0.1.1: fixed datetime stamp for Europe/Zurich tz
 * v0.1: initial release
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# twdlstm train v0.1.1
+# twdlstm train v0.2
 
 import sys # CLI argumennts: print(sys.argv)
 import os # os.getcwd, os.chdir
@@ -37,14 +37,14 @@ colvec = [
 # plt.close()
 
 
-#%% load tstoy data, just three series for now TODO:loop over supplied series
+#%% load tstoy data, loop over series and stack
 path_tstoy = config['path_data'] + '/tstoy' + config['tstoy'] + '/'
 # '/mydata/forestcast/william/WP3/DataProcessed/tstoy04/'
 
 # now = datetime.now() # UTC by def on runai
 now = datetime.now(tz=ZoneInfo("Europe/Zurich"))
 now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-print(now_str + ' running twdlstm train v0.1.1\n')
+print(now_str + ' running twdlstm train v0.2\n')
 # print('\n')
 
 print('Supplied config:')
@@ -52,192 +52,124 @@ print(path_config+'\n')
 # print('\n')
 
 seriesvec = config['series']
-
-series = seriesvec[0] # '01' # 01-42
-path_csv_series = (path_tstoy + 'SeparateSeries/tstoy' + config['tstoy'] +
-    '_series_' + series + '.csv'
-)
-dat_01 = pd.read_csv(
-    path_csv_series,
-    header=0,
-    # nrows=2 # to check cols
-    dtype={
-        'ts':str,
-        'twd':float,
-        'pr':float,
-        'at':float,
-        'ws':float,
-        'dp':float,
-        'sr':float,
-        'lr':float
-    }
-)
-
-series = seriesvec[1]
-path_csv_series = (path_tstoy + 'SeparateSeries/tstoy' + config['tstoy'] +
-    '_series_' + series + '.csv'
-)
-dat_02 = pd.read_csv(
-    path_csv_series,
-    header=0,
-    # nrows=2 # to check cols
-    dtype={
-        'ts':str,
-        'twd':float,
-        'pr':float,
-        'at':float,
-        'ws':float,
-        'dp':float,
-        'sr':float,
-        'lr':float
-    }
-)
-
-series = seriesvec[2]
-path_csv_series = (path_tstoy + 'SeparateSeries/tstoy' + config['tstoy'] +
-    '_series_' + series + '.csv'
-)
-dat_03 = pd.read_csv(
-    path_csv_series,
-    header=0,
-    # nrows=2 # to check cols
-    dtype={
-        'ts':str,
-        'twd':float,
-        'pr':float,
-        'at':float,
-        'ws':float,
-        'dp':float,
-        'sr':float,
-        'lr':float
-    }
-)
-
-# print(dat_01.shape) # dimensions: rows and cols
-# print(dat_02.shape) # dimensions: rows and cols
-# print(dat_03.shape) # dimensions: rows and cols
-
-
-
-#%% set tr subset with beginning of tr tw
-# nT = dat_01.shape[0] # total nb time points
-# print(nT)
-
-ind_t0 = int(np.where(dat_01['ts']==config['date_t0'])[0].item())
-# ^ dat row index corresponding to date_t0 in config
-# print(dat_01.iloc[ind_t0,:]) # check
-
+covvec = config['covvec']
 nT_tr = config['nT_tr'] # size of tr set
 nT_va = config['nT_va'] # size of va set
 
-# maxlag = 0 #
-# shift = 0
-# n_cont = 0
+# ini: s=0
+s = 0
+
+series_s = seriesvec[s] # '01' # 01-42
+path_csv_series_s = (path_tstoy + 'SeparateSeries/tstoy' + config['tstoy'] +
+    '_series_' + series_s + '.csv'
+)
+dat_s = pd.read_csv(
+    path_csv_series_s,
+    header=0,
+    # nrows=2 # to check cols
+    dtype={
+        'ts':str,
+        'twd':float,
+        'pr':float,
+        'at':float,
+        'ws':float,
+        'dp':float,
+        'sr':float,
+        'lr':float
+    }
+)
+
+ind_t0 = int(np.where(dat_s['ts']==config['date_t0'])[0].item())
+# ^ dat row index corresponding to date_t0 in config
 
 ind_tr = range(ind_t0, nT_tr+ind_t0, 1) # for i in ind_tr: print(i)
 ind_va = range(nT_tr+ind_t0, nT_tr+ind_t0+nT_va, 1) # for i in ind_va: print(i)
 
-# ind_shift_tr = range(ind_t0-shift-n_cont, ind_t0-shift+nT_tr, 1)
-# ind_shift_va = range(ind_t0-shift-n_cont+nT_tr, ind_t0-shift+nT_tr+nT_va, 1)
-# # ind_tr is the ref, shift and c_cont only for cov
+y_full = dat_s['twd']
+y_tr = y_full[ind_tr]
+y_va = y_full[ind_va]
 
-y_01_full = dat_01['twd']
-y_01_tr = y_01_full[ind_tr]
-y_01_va = y_01_full[ind_va]
+x_full = dat_s[covvec]
+x_tr = x_full.iloc[ind_tr,:] # ini, time subset, all cols
+x_va = x_full.iloc[ind_va,:] # ini, time subset, all cols
 
-y_02_full = dat_02['twd']
-y_02_tr = y_02_full[ind_tr]
-y_02_va = y_02_full[ind_va]
+mean_s = np.apply_along_axis(np.mean, 0, x_tr)
+sd_s = np.apply_along_axis(np.std, 0, x_tr)
+for j in range(x_tr.shape[1]): # loop over columns, overwrite each cov
+    x_tr.iloc[:,j] = (x_tr.iloc[:,j]-mean_s[j])/sd_s[j]
+    x_va.iloc[:,j] = (x_va.iloc[:,j]-mean_s[j])/sd_s[j]
 
-y_03_full = dat_03['twd']
-y_03_tr = y_03_full[ind_tr]
-y_03_va = y_03_full[ind_va]
+x_tr = np.expand_dims(x_tr, axis=0) # add a dim for stacking series
+x_va = np.expand_dims(x_va, axis=0) # add a dim for stacking series
+y_tr = np.expand_dims(y_tr, axis=0) # add a dim for stacking series
+y_va = np.expand_dims(y_va, axis=0) # add a dim for stacking series
 
-# print(y_01_tr.isna().any())
-# print(y_01_va.isna().any())
-# print(y_02_tr.isna().any())
-# print(y_02_va.isna().any())
-# print(y_03_tr.isna().any())
-# print(y_03_va.isna().any())
-# # ^ all have no NAs, ok
+for s in range(1,len(seriesvec)): # loop over series after 1st 
+    series_s = seriesvec[s] # '01' # 01-42
+    path_csv_series_s = (path_tstoy + 'SeparateSeries/tstoy' + config['tstoy'] +
+        '_series_' + series_s + '.csv'
+    )
+    dat_s = pd.read_csv(
+        path_csv_series_s,
+        header=0,
+        dtype={
+            'ts':str,
+            'twd':float,
+            'pr':float,
+            'at':float,
+            'ws':float,
+            'dp':float,
+            'sr':float,
+            'lr':float
+        }
+    )
+    
+    y_s_full = dat_s['twd']
+    y_s_tr = y_s_full[ind_tr]
+    y_s_va = y_s_full[ind_va]
+    
+    x_s_full = dat_s[covvec]
+    x_s_tr = x_s_full.iloc[ind_tr,:] # ini, time subset, all cols
+    x_s_va = x_s_full.iloc[ind_va,:] # ini, time subset, all cols
+    
+    mean_s = np.apply_along_axis(np.mean, 0, x_s_tr)
+    sd_s = np.apply_along_axis(np.std, 0, x_s_tr)
+    for j in range(x_s_tr.shape[1]): # loop over columns, overwrite each cov
+        x_s_tr.iloc[:,j] = (x_s_tr.iloc[:,j]-mean_s[j])/sd_s[j]
+        x_s_va.iloc[:,j] = (x_s_va.iloc[:,j]-mean_s[j])/sd_s[j]
+    
+    x_s_tr = np.expand_dims(x_s_tr, axis=0) # add a dim for stacking series
+    x_s_va = np.expand_dims(x_s_va, axis=0) # add a dim for stacking series
+    y_s_tr = np.expand_dims(y_s_tr, axis=0) # add a dim for stacking series
+    y_s_va = np.expand_dims(y_s_va, axis=0) # add a dim for stacking series
+       
+    x_tr = np.concatenate((x_tr, x_s_tr), axis=0)
+    x_va = np.concatenate((x_va, x_s_va), axis=0)
+    y_tr = np.concatenate((y_tr, y_s_tr), axis=0)
+    y_va = np.concatenate((y_va, y_s_va), axis=0)
 
-covvec = config['covvec']
+# end loop over s in seriesvec
 
-x_01_full = dat_01[covvec]
-x_02_full = dat_02[covvec]
-x_03_full = dat_03[covvec]
-x_01_tr = x_01_full.iloc[ind_tr,:] # ini, time subset, all cols
-x_01_va = x_01_full.iloc[ind_va,:] # ini, time subset, all cols
-x_02_tr = x_02_full.iloc[ind_tr,:] # ini, time subset, all cols
-x_02_va = x_02_full.iloc[ind_va,:] # ini, time subset, all cols
-x_03_tr = x_03_full.iloc[ind_tr,:] # ini, time subset, all cols
-x_03_va = x_03_full.iloc[ind_va,:] # ini, time subset, all cols
+# x_tr.shape
+# x_va.shape
+# y_tr.shape
+# y_va.shape
 
-# print(x_01_tr.isna().any())
-# print(x_02_tr.isna().any())
-# print(x_03_tr.isna().any())
-# # ^ ok, no NAs
+# x_normalized = True
 
-# x_normalized = False
-# y_01_tr.shape
-# x_01_tr.shape
-# y_01_va.shape
-# x_01_va.shape
-# # ^ nb cols for x = len(covvec)*(maxlag+1)
+xtr = torch.tensor(x_tr, dtype=torch.float32)
+xva = torch.tensor(x_va, dtype=torch.float32)
+ytr = torch.tensor(y_tr, dtype=torch.float32) # .reshape(-1,1)
+yva = torch.tensor(y_va, dtype=torch.float32)
 
-# plt.figure(figsize=(12,6))
-# plt.scatter(ind_tr, y_01_tr, s=16, c=colvec[0], label='tr')
-# plt.scatter(ind_va, y_01_va, s=16, c=colvec[1], label='va')
-# plt.legend(loc='upper left')
-# plt.title('series 01')
-# plt.savefig('plot.pdf')
-# plt.close()
-
-
-
-
-
-# #%% normalize input features for mean=0 and var=1
-mean_01 = np.apply_along_axis(np.mean, 0, x_01_tr)
-mean_02 = np.apply_along_axis(np.mean, 0, x_02_tr)
-mean_03 = np.apply_along_axis(np.mean, 0, x_03_tr)
-sd_01 = np.apply_along_axis(np.std, 0, x_01_tr)
-sd_02 = np.apply_along_axis(np.std, 0, x_02_tr)
-sd_03 = np.apply_along_axis(np.std, 0, x_03_tr)
-
-for j in range(x_01_tr.shape[1]): # loop over columns, overwrite each cov
-    x_01_tr.iloc[:,j] = (x_01_tr.iloc[:,j]-mean_01[j])/sd_01[j]
-    x_01_va.iloc[:,j] = (x_01_va.iloc[:,j]-mean_01[j])/sd_01[j]
-    x_02_tr.iloc[:,j] = (x_02_tr.iloc[:,j]-mean_02[j])/sd_02[j]
-    x_02_va.iloc[:,j] = (x_02_va.iloc[:,j]-mean_02[j])/sd_02[j]
-    x_03_tr.iloc[:,j] = (x_03_tr.iloc[:,j]-mean_03[j])/sd_03[j]
-    x_03_va.iloc[:,j] = (x_03_va.iloc[:,j]-mean_03[j])/sd_03[j]
-
-x_normalized = True
-
-x01tr = torch.tensor(x_01_tr.values, dtype=torch.float32)
-x02tr = torch.tensor(x_02_tr.values, dtype=torch.float32)
-x03tr = torch.tensor(x_03_tr.values, dtype=torch.float32)
-x01va = torch.tensor(x_01_va.values, dtype=torch.float32)
-x02va = torch.tensor(x_02_va.values, dtype=torch.float32)
-x03va = torch.tensor(x_03_va.values, dtype=torch.float32)
-
-y01tr = torch.tensor(y_01_tr.values, dtype=torch.float32).reshape(-1,1)
-y02tr = torch.tensor(y_02_tr.values, dtype=torch.float32).reshape(-1,1)
-y03tr = torch.tensor(y_03_tr.values, dtype=torch.float32).reshape(-1,1)
-y01va = torch.tensor(y_01_va.values, dtype=torch.float32).reshape(-1,1)
-y02va = torch.tensor(y_02_va.values, dtype=torch.float32).reshape(-1,1)
-y03va = torch.tensor(y_03_va.values, dtype=torch.float32).reshape(-1,1)
-
-# y01tr.shape
-# x01tr.shape
-# y01va.shape
-# x01va.shape
-
+# xtr.shape
+# xva.shape
+# ytr.shape
+# yva.shape
 
 
 #%% LSTM  model class
-i_size = x01tr.shape[1] # nb cols in xtr = nb input features 
+i_size = xtr.shape[2] # nb cols in xtr = nb input features 
 h_size = config['h_size']
 o_size = config['o_size']
 nb_layers = config['nb_layers']
@@ -353,6 +285,22 @@ maxepoch = int(config['maxepoch'])
 # TODO: add scheduler
 
 
+# a = torch.tensor(([1,2,3],[4,5,6]))
+# a.shape
+# b = torch.tensor(([7,8,9],[10,11,12]))
+# b.shape
+
+# c = torch.stack((a, b), dim=a.dim())
+# c.shape
+# c[:,:,1]
+# b
+
+# d = torch.tensor(([13,14,15],[16,17,18]))
+# d.shape
+
+# e = torch.cat((c,d),dim=d.dim())
+
+
 #%% optim
 epoch = 0
 lossvec_tr = []
@@ -364,34 +312,20 @@ while (epoch < maxepoch) :
     optimizer.zero_grad()
     loss_tr = 0.0 # just to display
     loss_va = 0.0 # record va loss
-    # for j in range(3): # loop over all tr obs
-    # 01
-    fwdpass = model(x01tr,(h0,c0)) # from ini
-    y_pred = fwdpass[0] # whole series at once
-    losstr = loss_fn(y_pred, y01tr)
-    loss_tr += losstr.item()
-    losstr.backward() # accumulate grad over series
-    with torch.no_grad():
-        yva_pred = model(x01va, fwdpass[1])[0]
-        loss_va += loss_fn(yva_pred, y01va).item()
-    # 02
-    fwdpass = model(x02tr,(h0,c0)) # from ini
-    y_pred = fwdpass[0] # whole series at once
-    losstr = loss_fn(y_pred, y02tr)
-    loss_tr += losstr.item()
-    losstr.backward() # accumulate grad over series
-    with torch.no_grad():
-        yva_pred = model(x01va, fwdpass[1])[0]
-        loss_va += loss_fn(yva_pred, y01va).item()
-    # 03
-    fwdpass = model(x03tr,(h0,c0)) # from ini
-    y_pred = fwdpass[0] # whole series at once
-    losstr = loss_fn(y_pred, y03tr)
-    loss_tr += losstr.item()
-    losstr.backward() # accumulate grad over series
-    with torch.no_grad():
-        yva_pred = model(x01va, fwdpass[1])[0]
-        loss_va += loss_fn(yva_pred, y01va).item()
+    
+    for s in range(len(seriesvec)): # loop over series (dim 0)
+        xtr_s = torch.select(xtr, dim=0, index=s)
+        ytr_s = torch.select(ytr, dim=0, index=s).reshape(-1,1)
+        fwdpass = model(xtr_s, (h0,c0)) # from ini
+        y_pred = fwdpass[0] # whole series at once
+        losstr = loss_fn(y_pred, ytr_s)
+        loss_tr += losstr.item()
+        losstr.backward() # accumulate grad over series
+        with torch.no_grad():
+            xva_s = torch.select(xva, dim=0, index=s)
+            yva_s = torch.select(yva, dim=0, index=s).reshape(-1,1)
+            yva_pred = model(xva_s, fwdpass[1])[0]
+            loss_va += loss_fn(yva_pred, yva_s).item()
     
     optimizer.step() # over all series
     # scheduler.step() # update lr throughout epochs
@@ -432,21 +366,38 @@ plt.close()
 
 model.eval()
 
-fwdpass_tr = model(x01tr,(h0,c0)) # from ini over all ts
+s = 0 # 1st series as ref, plot only this one
+xtr_s = torch.select(xtr, dim=0, index=s)
+xva_s = torch.select(xva, dim=0, index=s)
+ytr_s = torch.select(ytr, dim=0, index=s).reshape(-1,1).detach().numpy()
+yva_s = torch.select(yva, dim=0, index=s).reshape(-1,1).detach().numpy()
+fwdpass_tr = model(xtr_s, (h0,c0)) # from ini over all ts
 ytr_pred = fwdpass_tr[0].detach().numpy() #
-yva_pred = model(x01va, fwdpass_tr[1])[0].detach().numpy() #
-print('Series',seriesvec[0],'tr R^2 =',round(r2_score(y_01_tr, ytr_pred),4)) # R^2 on training set
-print('Series',seriesvec[0],'va R^2 =',round(r2_score(y_01_va, yva_pred),4)) # R^2 on validation set
+yva_pred = model(xva_s, fwdpass_tr[1])[0].detach().numpy() #
+print('Series',seriesvec[s],'tr R^2 =',round(r2_score(ytr_s, ytr_pred),4)) # R^2 on training set
+print('Series',seriesvec[s],'va R^2 =',round(r2_score(yva_s, yva_pred),4)) # R^2 on validation set
 
 plt.figure(figsize=(12,6))
-plt.scatter(ind_tr, y_01_tr, s=16, c=colvec[0], label='tr')
-plt.scatter(ind_va, y_01_va, s=16, c=colvec[1], label='va')
+plt.scatter(ind_tr, ytr_s, s=16, c=colvec[0], label='tr')
+plt.scatter(ind_va, yva_s, s=16, c=colvec[1], label='va')
 plt.plot(ind_tr, ytr_pred, linewidth=1, color=colvec[0])
 plt.plot(ind_va, yva_pred, linewidth=1, color=colvec[1])
 plt.legend(loc='upper left')
-plt.title('series ' + seriesvec[0])
-plt.savefig(path_out + '_pred_series' + seriesvec[0] + '.pdf')
+plt.title('series ' + seriesvec[s])
+plt.savefig(path_out + '_pred_series' + seriesvec[s] + '.pdf')
 plt.close()
+
+for s in range(1,len(seriesvec)): # loop over series (dim 0)
+    xtr_s = torch.select(xtr, dim=0, index=s)
+    xva_s = torch.select(xva, dim=0, index=s)
+    ytr_s = torch.select(ytr, dim=0, index=s).reshape(-1,1).detach().numpy()
+    yva_s = torch.select(yva, dim=0, index=s).reshape(-1,1).detach().numpy()
+    fwdpass_tr = model(xtr_s, (h0,c0)) # from ini over all ts
+    ytr_pred = fwdpass_tr[0].detach().numpy() #
+    yva_pred = model(xva_s, fwdpass_tr[1])[0].detach().numpy() #
+    print('Series',seriesvec[s],'tr R^2 =',round(r2_score(ytr_s, ytr_pred),4)) # R^2 on training set
+    print('Series',seriesvec[s],'va R^2 =',round(r2_score(yva_s, yva_pred),4)) # R^2 on validation set
+
 
 print('\n')
 print('done')
